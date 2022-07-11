6496d0684a3a689a6645929c0b48d7084affb55f
mrsdno <christine@dno.dev>
2022-07-11 15:34:42 -0400

    updated social worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,7 @@ const FILES_TO_CACHE = [
     "./index.html",
     "./js/index.js",
     "./css/styles.css",
-    "./service-worker.js"
+  "./service-worker.js"
 ];
 
 const APP_PREFIX = "BudgetTracker-";
@@ -40,42 +40,20 @@ self.addEventListener('activate', function(evt) {
 });
 
 // Intercept fetch requests
-self.addEventListener('fetch', function(evt) {
-  if (evt.request.url.includes('/api/')) {
-    evt.respondWith(
-      caches
-        .open(CACHE_NAME)
-        .then(cache => {
-          return fetch(evt.request)
-            .then(response => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
 
-              return response;
-            })
-            .catch(err => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
-        })
-        .catch(err => console.log(err))
-    );
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log(' responding with cache: ' + e.request.url);
+        return request;
+      } else {
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
 
-    return;
-  }
-
-  evt.respondWith(
-    fetch(evt.request).catch(function() {
-      return caches.match(evt.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (evt.request.headers.get('accept').includes('text/html')) {
-          // return the cached home page for all requests for html pages
-          return caches.match('/');
-        }
-      });
     })
-  );
+
+  )
 });
